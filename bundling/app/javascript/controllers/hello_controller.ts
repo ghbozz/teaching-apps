34e07a0e7b277fd4954e10ea8586cf3422186573
romain sanson <romain.sanson@hey.com>
2022-05-03 23:46:13 +0200

    cleanup

--- bundling/app/javascript/controllers/hello_controller.ts
+++ bundling/app/javascript/controllers/hello_controller.ts
@@ -2,7 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect(): void {
-    // this.element.textContent = "Hello again !!!"
+    interface Person {
+      name: string;
+      age: number;
+      email: string;
+    }
+
+    const person: Person = {
+      name: 'John',
+      age: '30',
+      email: 'john@gmail.com'
+    }
+
+    console.log(person);
   }
 
   click(): void {
