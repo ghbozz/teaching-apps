cd41e5f3b5907bfad1707cebce35455fbf552bd6
romain sanson <romain.sanson@hey.com>
2022-05-28 08:23:57 +0200

    turbo stream

--- rpuw/app/controllers/games_controller.rb
+++ rpuw/app/controllers/games_controller.rb
@@ -20,10 +20,23 @@ class GamesController < ApplicationController
     @question = Question.find(params[:question_id])
     @answer = Answer.find(params[:answer_id])
     @participant = User.find(params[:participant_id])
-    @next_question = @game.questions.where.not(id: @question.id).first
+    @next_question = @game.next_question(@question)
 
     if @answer.correct?
-      render turbo_stream: turbo_stream.replace(:question, partial: "games/question", locals: { question: @next_question })
+      broadcast_next_question unless @game.last_question?(@question)
     end
+  end
+
+  private 
+
+  def broadcast_next_question
+    Turbo::StreamsChannel.broadcast_update_to @game, 
+      target: "game_#{@game.id}", 
+      partial: "games/question", 
+      locals: { 
+        game: @game, 
+        question: @next_question,
+        participant: @participant
+      }
   end
 end
--- rpuw/app/models/game.rb
+++ rpuw/app/models/game.rb
@@ -3,4 +3,12 @@ class Game < ApplicationRecord
   has_many :participants, through: :user_games, source: :user
   has_many :game_questions, dependent: :destroy
   has_many :questions, through: :game_questions
+
+  def next_question(question)
+    questions[questions.index(question) + 1]
+  end
+
+  def last_question?(question)
+    questions.last == question
+  end
 end
